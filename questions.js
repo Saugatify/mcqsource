7889a4e45eb61b3acb7eb07386ef2728358a4e42
Saugat Ghimire <saugatg189@gmail.com>
2023-01-21 16:54:38 +0545

    Version 1.0

---
+++ questions.js
@@ -0,0 +1,93 @@
+var questions = [
+    {
+      
+        question: "1", 
+        answer: "17.7",
+        options: [
+            "17.7347",
+            "17.7347",
+            "17.73",
+            "17.7"
+
+        ]
+    },
+    {
+        
+        question: "2 The area of this sheet up to four correct significant figures is",
+        answer: "9.378 m2",
+        options: [
+            "9.376 m2",
+            "9.378 m2",
+            "9.379 m2",
+            "9.388 m2"
+
+        ]
+    },
+    {
+        num: 3,
+        question: "3um horizontal range of a projectile is 400 m. The maximum value of height attained by it will be?",
+        answer: "200",
+        options: [
+            "100",
+            "200",
+            "400",
+            "800"
+
+        ]
+    },
+    {
+        num: 4,
+        question: "4 imum horizontal range of a projectile is 400 m. The maximum value of height attained by it will be?",
+        answer: "200",
+        options: [
+            "100",
+            "200",
+            "400",
+            "800"
+
+        ]
+    },
+    {
+        num: 5,
+        question: "5 ge of a projectile is 400 m. The maximum value of height attained by it will be?",
+        answer: "200",
+        options: [
+            "100",
+            "200",
+            "400",
+            "800"
+
+        ]
+    },
+    {
+        num: 6,
+        question: "6 of a projectile is 400 m. The maximum value of height attained by it will be?",
+        answer: "200",
+        options: [
+            "100",
+            "200",
+            "400",
+            "800"
+
+        ]
+    },
+
+
+    {
+        num: 7,
+        question: "777projectile is 400 m. The maximum value of height attained by it will be?",
+        answer: "200",
+        options: [
+            "100",
+            "200",
+            "400",
+            "800"
+
+        ]
+    },
+
+
+
+
+
+];
